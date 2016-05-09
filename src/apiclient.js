932119325b4ce66fd6d6252db2037ebf50cb4c0c
Lars Kristian Maron Telle <lars.kristian.maron.telle@blankoslo.no>
2016-05-09 13:53:24 +0200

    Add apiclient with getProjects function

---
+++ src/apiclient.js
@@ -0,0 +1,9 @@
+import axios_ from 'axios';
+
+const axios = axios_.create({
+  baseURL: 'https://api-dev.floq.no/',
+  timeout: 1000,
+  headers: { Authorization: `Bearer ${apiToken}` }
+});
+
+export const getProjects = () => axios.get('/projects');
