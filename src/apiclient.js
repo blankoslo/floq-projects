1e4fc9c9550931b9e63005b8a65e596e1068455b
Lars Kristian Maron Telle <lars.kristian.maron.telle@blankoslo.no>
2016-05-10 15:19:33 +0200

    Prefer singular responses

--- src/apiclient.js
+++ src/apiclient.js
@@ -3,7 +3,7 @@ import axios_ from 'axios';
 const axios = axios_.create({
   baseURL: config.apiUri,
   timeout: 1000,
-  headers: { Authorization: `Bearer ${apiToken}` }
+  headers: { Authorization: `Bearer ${apiToken}`, Prefer: 'plurality=singular' }
 });
 
 export const getProjects = () => axios.get('/projects?select=id,name,customer{*}');
