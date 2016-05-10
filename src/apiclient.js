6bf830e29b7ff56a60e56c866a3b071428032c21
Lars Kristian Maron Telle <lars.kristian.maron.telle@blankoslo.no>
2016-05-10 15:48:48 +0200

    Only set plurality when getting single project

--- src/apiclient.js
+++ src/apiclient.js
@@ -3,9 +3,11 @@ import axios_ from 'axios';
 const axios = axios_.create({
   baseURL: config.apiUri,
   timeout: 1000,
-  headers: { Authorization: `Bearer ${apiToken}`, Prefer: 'plurality=singular' }
+  headers: { Authorization: `Bearer ${apiToken}` }
 });
 
 export const getProjects = () => axios.get('/projects?select=id,name,customer{*}');
 
-export const getProject = (id) => axios.get(`/projects?id=eq.${id}`);
+export const getProject = (id) => axios.get(`/projects?id=eq.${id}`, {
+  headers: { Prefer: 'plurality=singular' }
+});
