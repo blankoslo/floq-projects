e030581d9226103144c9ab699f4392872986e8e4
Lars Kristian Maron Telle <lars.kristian.maron.telle@blankoslo.no>
2016-05-12 15:21:23 +0200

    Change order of routes
    - Don't catch "new" as id.

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import ProjectContainer from './containers/projectcontainer';
 export default (
   <Router>
     <Route path='/projects' component={ProjectsListContainer} />
-    <Route path='/projects/:id' component={ProjectContainer} />
     <Route path='/projects/new' component={ProjectContainer} />
+    <Route path='/projects/:id' component={ProjectContainer} />
   </Router>
 );
