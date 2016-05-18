e6f4085d8037340d3986c3e417742df3b0588e93
Erlend Hamberg <eh@blankoslo.no>
2016-05-18 10:24:44 +0200

    Replace project with the returned, updated entity
    After updating a project the API server will respond with the full, updated
    entity with which we can replace our state's `project`.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -16,7 +16,7 @@ const projectReducer = (previousState = {}, action) => {
     case FETCH_PROJECT:
       return action.payload.data;
     case UPDATE_PROJECT:
-      return Object.assign(previousState, action.meta);
+      return Object.assign(previousState, action.payload.data);
     default:
       return previousState;
   }
