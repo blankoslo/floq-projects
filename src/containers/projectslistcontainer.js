e835558c6442bbc2605784e6d8cc21b22dd5aff4
Lars Kristian Maron Telle <lars.kristian.maron.telle@blankoslo.no>
2016-05-23 09:26:41 +0200

    Added and removed comments

--- src/containers/projectslistcontainer.js
+++ src/containers/projectslistcontainer.js
@@ -5,7 +5,7 @@ import ProjectsList from '../components/projectslist';
 import { fetchProjects } from '../actions/index';
 
 class ProjectsListContainer extends Component {
-  constructor(props) {
+  constructor(props) {    // props kommer fra Redux? Er det slik at props == Redux store?
     super(props);
 
     this.props.dispatch(fetchProjects());
@@ -25,4 +25,7 @@ const mapStateToProps = (state) => ({
   projects: state.projects // state.projects
 });
 
+/*
+  Connects the Redux store to the React component ProjectsListContainer.
+*/
 export default connect(mapStateToProps)(ProjectsListContainer);
