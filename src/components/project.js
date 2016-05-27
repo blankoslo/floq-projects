38db3feb577082e828374911660dad1a33b4aad7
Erlend Hamberg <eh@blankoslo.no>
2016-05-27 17:48:11 +0200

    Create a valid start state for project editor for a new project

--- src/components/project.js
+++ src/components/project.js
@@ -1,15 +1,19 @@
+import * as _ from 'lodash';
 import React, { Component } from 'react';
 
 class Project extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { name: null, customer: null };
+    this.state = { name: '', customer: -1 };
   }
 
   componentWillReceiveProps(nextProps) {
-    if (nextProps.project !== {}) {
-      this.state = { name: nextProps.project.name, customer: nextProps.project.customer };
+    if (!_.isEmpty(nextProps.project)) {
+      this.setState({
+        name: nextProps.project.name,
+        customer: nextProps.project.customer
+      });
     }
   }
 
@@ -28,8 +32,12 @@ class Project extends Component {
       return <p>...loading</p>;
     }
 
-    const customerElements = this.props.customers.map((c) =>
+    let customerElements = this.props.customers.map((c) =>
       <option key={c.id} value={c.id}>{c.name}</option>);
+
+    customerElements.push(<option disabled hidden key='-1' value='-1'></option>);
+
+    const isNew = _.isEmpty(this.props.project);
 
     return (
       <div>
@@ -40,7 +48,7 @@ class Project extends Component {
           </div>
           <div>
             <label>Kunde: </label>
-            <select value={this.state.customer} onChange={this.onCustomerChange}>
+            <select disabled={!isNew} value={this.state.customer} onChange={this.onCustomerChange}>
               {customerElements}
             </select>
           </div>
