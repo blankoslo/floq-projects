bbdde50146d146292eb119d711bd313d7a3907ec
John Andre Hestad <jahestad@gmail.com>
2016-06-22 11:17:21 +0200

    Restructured setup var from object to function
    This way each test gets seperate data and won't touch eachothers state. Tests can now run in parallell.

--- test/project.spec.js
+++ test/project.spec.js
@@ -4,66 +4,80 @@ import expect from 'expect';
 import ProjectEditor from '../src/components/projectEditor';
 import Immutable from 'immutable';
 
-const form = {
-  loading: false,
-  data: new Immutable.Map({
-    customer: 1,
-    name: 'test_project_one'
-  })
-};
+// setup is a function, so that each call gets it's own objects (mutable state)
+const setup = () => {
+  const form = {
+    loading: false,
+    data: new Immutable.Map({
+      customer: 1,
+      name: 'test_project_one'
+    })
+  };
 
-const customers = {
-  loading: false,
-  data: new Immutable.Map([
-    {
-      id: 1,
-      name: 'test_customer_one'
-    },
-    {
-      id: 2,
-      name: 'test_customer_two'
-    }
-  ].map(e => [e.id, e]))
-};
+  const customers = {
+    loading: false,
+    data: new Immutable.Map([
+      {
+        id: 1,
+        name: 'test_customer_one'
+      },
+      {
+        id: 2,
+        name: 'test_customer_two'
+      }
+    ].map(e => [e.id, e]))
+  };
 
-const setup = {
-  form,
-  customers,
-  wrapper: shallow(
+  const actions = {
+    onSubmit: expect.createSpy(),
+    onChange: expect.createSpy()
+  };
+
+  const wrapper = shallow(
     <ProjectEditor
       form={form}
       customers={customers}
-      onSubmit={() => {}}
-      onChange={() => {}}
+      onSubmit={actions.onSubmit}
+      onChange={actions.onChange}
       isNew
-    />)
+    />);
+
+  return {
+    form,
+    customers,
+    actions,
+    wrapper
+  };
 };
 
 describe('<ProjectEditor />', () => {
   it('contains <input> with value equal to "form.data.name"', () => {
-    expect(setup.wrapper.find('input').props().value).toEqual(setup.form.data.get('name'));
+    const { wrapper, form } = setup();
+    expect(wrapper.find('input').props().value).toEqual(form.data.get('name'));
   });
 
   it('contains <select> with <option> containing all customers + hidden default', () => {
-    expect(setup.wrapper.find('select').props().children.length)
-    .toBe(setup.customers.data.size + 1);
+    const { wrapper, customers } = setup();
+    expect(wrapper.find('select').props().children.length)
+    .toBe(customers.data.size + 1);
   });
 
   it('contains <select> with prop "disabled=false" when isNew===true', () => {
-    expect(setup.wrapper.find('select').prop('disabled'))
+    const { wrapper } = setup();
+    expect(wrapper.find('select').prop('disabled'))
      .toBeFalsy();
   });
 
   it('contains <select> with prop "disabled=true" when isNew===true', () => {
+    const { customers, form } = setup();
     const wrapper = shallow(
       <ProjectEditor
-        form={setup.form}
-        customers={setup.customers}
+        form={form}
+        customers={customers}
         onSubmit={() => {}}
         onChange={() => {}}
         isNew={false}
       />);
-
     expect(wrapper.find('select').prop('disabled'))
      .toBeTruthy();
   });
