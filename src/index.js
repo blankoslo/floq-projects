a612aaea8539f85440f43d3d3df46309cc2f7cdc
John Andre Hestad <jahestad@gmail.com>
2016-06-28 19:17:03 +0200

    Added material-ui to main index.js
    Copied from floq-employee project.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import { Provider } from 'react-redux';
 import { compose, createStore, applyMiddleware } from 'redux';
 import apiMiddleware from './middleware/api';
 import { Router, browserHistory } from 'react-router';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import routes from './routes';
 import reducers from './reducers';
 
 require('../styles/main.less');
+
+// needed for material-ui, for now
+injectTapEventPlugin();
 
 const createStoreWithMiddleware = compose(
     applyMiddleware(apiMiddleware),
@@ -17,6 +23,8 @@ const createStoreWithMiddleware = compose(
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Router history={browserHistory} routes={routes} />
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+      <Router history={browserHistory} routes={routes} />
+    </MuiThemeProvider>
   </Provider>
   , document.getElementById('app'));
