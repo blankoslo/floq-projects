5a4a27d88dedb1ea1638fad634fa949fdcf21185
John Andre Hestad <jahestad@gmail.com>
2016-07-04 14:46:55 +0200

    Sort customers by name

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -49,17 +49,22 @@ const selectedProjectReducer = (previousState = null, action) => {
   }
 };
 
-const customersReducer = (previousState = { loading: true, data: new Immutable.Map() }, action) => {
+const lowerCaseName = c => c.name.toLowerCase();
+
+const customersReducer = (previousState = {
+  loading: true,
+  data: new Immutable.OrderedMap()
+}, action) => {
   switch (action.type) {
     case FETCH_CUSTOMERS:
       return {
         loading: false,
-        data: new Immutable.Map(action.payload.map(e => [e.id, e]))
+        data: new Immutable.OrderedMap(action.payload.map(e => [e.id, e])).sortBy(lowerCaseName)
       };
     case CREATE_CUSTOMER:
       return {
         loading: false,
-        data: previousState.data.set(action.payload.id, action.payload)
+        data: previousState.data.set(action.payload.id, action.payload).sortBy(lowerCaseName)
       };
     default:
       return previousState;
