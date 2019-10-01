f64999056aa432ced01098e21cf6f16f6b0a9952
Michael Eikeland <michael.eikeland@blank.no>
2019-10-01 13:46:54 +0200

    Add form validation for EditProject and plug ProjectsContext to API

--- src/common/api/ProjectAPI.ts
+++ src/common/api/ProjectAPI.ts
@@ -4,13 +4,13 @@ import { BaseAPI } from "./BaseAPI";
 
 const PROJECT_API_URL = `${BaseAPI.config.config.apiUri}/projects`;
 
-const GET_ALL_QUERY = new URLSearchParams({
-  select: "id,name,billable,customer,responsible,active",
-  order: "id.desc",
-}).toString();
-
 const getAll = (): Promise<Projects> =>
-  fetch(`${PROJECT_API_URL}?${GET_ALL_QUERY}`, BaseAPI.requestOptions)
+  fetch(
+    `${PROJECT_API_URL}?${new URLSearchParams({
+      order: "id.desc",
+    }).toString()}`,
+    BaseAPI.requestOptions
+  )
     .then(res => res.json())
     .then(res => {
       if (IsValidProjects(res)) {
@@ -22,7 +22,6 @@ const getAll = (): Promise<Projects> =>
 const get = (id: Project["id"]): Promise<Project> =>
   fetch(
     `${PROJECT_API_URL}?${new URLSearchParams({
-      select: "id,name,billable,customer,responsible,active",
       id: `eq.${id}`,
     }).toString()}`,
     BaseAPI.requestOptions
@@ -35,7 +34,42 @@ const get = (id: Project["id"]): Promise<Project> =>
       return Promise.reject("Response does not validate");
     });
 
+const create = (dto: Project): Promise<Project> =>
+  fetch(`${PROJECT_API_URL}`, {
+    ...BaseAPI.requestOptions,
+    method: "POST",
+    body: JSON.stringify(dto),
+  })
+    .then(res => res.json())
+    .then(res => {
+      if (IsValidProject(res)) {
+        return Promise.resolve(res);
+      }
+      return Promise.reject("Response does not validate");
+    });
+
+const update = (id: Project["id"], dto: Project): Promise<Project> =>
+  fetch(
+    `${PROJECT_API_URL}?${new URLSearchParams({
+      id: `eq.${id}`,
+    }).toString()}`,
+    {
+      ...BaseAPI.requestOptions,
+      method: "PATCH",
+      body: JSON.stringify(dto),
+    }
+  )
+    .then(res => res.json())
+    .then(res => {
+      if (IsValidProjects(res) && res.length === 1) {
+        return Promise.resolve(res[0]);
+      }
+      return Promise.reject("Response does not validate");
+    });
+
 export const ProjectAPI = {
   getAll,
   get,
+  create,
+  update,
 };
--- src/features/projects/components/EditProjectDialog.tsx
+++ src/features/projects/components/EditProjectDialog.tsx
@@ -1,24 +1,40 @@
 import {
   Button,
-  CircularProgress,
   Dialog,
   DialogActions,
   DialogContent,
   DialogTitle,
-  FormControl,
   FormControlLabel,
   Grid,
-  InputLabel,
   MenuItem,
-  Select,
-  Switch,
-  TextField,
+  TextField as MUITextField,
 } from "@material-ui/core";
 import { useCustomers } from "common/context/CustomersContext";
 import { useEmployees } from "common/context/EmployeesContext";
 import { useProjects } from "common/context/ProjectsContext";
-import React, { useEffect, useState } from "react";
-import { Project } from "types/Project";
+import { Field, Form, Formik, FormikActions, FormikProps } from "formik";
+import { Switch, TextField } from "formik-material-ui";
+import React from "react";
+import { Billable, Project } from "types/Project";
+import * as yup from "yup";
+
+const EditProjectSchema = yup.object().shape({
+  active: yup
+    .boolean()
+    .required("Et prosjekt må enten være aktivt eller inaktivt"),
+  id: yup.string().required("Et prosjekt må ha en prosjektkode"),
+  name: yup.string().required("Et prosjekt må ha et navn"),
+  billable: yup
+    .string()
+    .oneOf(["billable", "nonbillable", "unavailable"] as Billable[])
+    .required("Et prosjekt må ha fakturerbarhet"),
+  responsible: yup
+    .number()
+    .integer()
+    .required("Et prosjekt må ha en ansvarlig"),
+});
+
+type FormikProject = yup.InferType<typeof EditProjectSchema>;
 
 interface Props {
   projectId: Project["id"];
@@ -26,11 +42,13 @@ interface Props {
   onClose: () => void;
 }
 
-const billableElements = [
+const mapBillable: { value: Billable; name: string }[] = [
   { value: "billable", name: "Fakturerbart prosjekt" },
   { value: "nonbillable", name: "Ikke-fakturerbart prosjekt" },
   { value: "unavailable", name: "Utilgjengelig tid" },
-].map(c => (
+];
+
+const billableElements = mapBillable.map(c => (
   <MenuItem key={c.value} value={c.value}>
     {c.name}
   </MenuItem>
@@ -39,7 +57,6 @@ const billableElements = [
 const EditProjectDialog: React.FC<Props> = (props: Props) => {
   const { projectId, isOpen, onClose } = props;
   if (!isOpen) {
-    console.log("Not open");
     return null;
   }
 
@@ -62,109 +79,124 @@ const EditProjectDialog: React.FC<Props> = (props: Props) => {
     return null;
   }
 
-  const onSave = (): void => {
-    if (project) {
-      ctxProjects.actions.update(project.id, {
-        ...project,
-        name: "JUST TESTING",
-      });
-    }
-    onClose();
-  };
-
-  const employeeElements = ctxEmployees.data.map(e => ({
+  const mapEmployees = ctxEmployees.data.map(e => ({
     value: e.id,
-    label: [e.first_name, e.last_name, e.emoji].filter(w => !!w).join(" "),
+    name: [e.first_name, e.last_name, e.emoji].filter(Boolean).join(" "),
   }));
 
-  const [selected, setSelected] = useState("");
-  useEffect(() => {
-    if (project && project.responsible) {
-      setSelected(project.responsible.toString());
-    }
-  }, [project]);
+  const employeeElements = mapEmployees.map(c => (
+    <MenuItem key={c.value} value={c.value}>
+      {c.name}
+    </MenuItem>
+  ));
+
+  const onSubmit = (
+    values: FormikProject,
+    actions: FormikActions<FormikProject>
+  ): void => {
+    actions.setSubmitting(true);
+    const updated: Project = {
+      ...project,
+      ...values,
+      billable: values.billable as Billable,
+    };
+    ctxProjects.actions.update(project.id, updated).then(res => {
+      console.log(res);
+      actions.setSubmitting(false);
+      onClose();
+    });
+  };
 
   return (
     <Dialog open={isOpen} onClose={onClose} fullWidth>
-      {!project && <CircularProgress variant="indeterminate" />}
-      {project && (
-        <>
-          <DialogTitle>
-            {customer.name}: {project.name}
-          </DialogTitle>
-          <DialogContent>
-            <Grid container direction="column">
-              <FormControlLabel
-                control={<Switch checked={project.active} color="primary" />}
-                label="Aktiv"
-              />
+      <DialogTitle>
+        {customer.name}: {project.name}
+      </DialogTitle>
+      <Formik
+        initialValues={EditProjectSchema.cast(project)}
+        validationSchema={EditProjectSchema}
+        onSubmit={onSubmit}
+        render={(formikProps: FormikProps<FormikProject>): React.ReactNode => (
+          <Form>
+            <DialogContent>
+              <Grid container direction="column">
+                <FormControlLabel
+                  control={
+                    <Field
+                      label="Aktiv"
+                      name="active"
+                      component={Switch}
+                      color="primary"
+                    />
+                  }
+                  label="Aktiv"
+                />
 
-              <TextField
-                margin="dense"
-                type="text"
-                name="customer"
-                label="Kunde"
-                value={customer.name}
-                disabled
-              />
+                <MUITextField
+                  type="text"
+                  label="Kunde"
+                  value={customer.name}
+                  margin="dense"
+                  disabled
+                />
 
-              <TextField
-                margin="dense"
-                type="text"
-                name="id"
-                label="Prosjektkode"
-                value={project.id}
-                disabled
-              />
+                <Field
+                  type="text"
+                  name="id"
+                  label="Prosjektkode"
+                  component={TextField}
+                  margin="dense"
+                  disabled
+                />
 
-              <TextField
-                margin="dense"
-                type="text"
-                name="name"
-                label="Prosjekt"
-                value={project.name}
-              />
+                <Field
+                  type="text"
+                  name="name"
+                  label="Prosjekt"
+                  component={TextField}
+                  margin="dense"
+                />
 
-              <FormControl>
-                <InputLabel htmlFor="billable">Fakturerbar</InputLabel>
-                <Select
-                  value={project.billable}
+                <Field
+                  type="text"
+                  name="billable"
+                  label="Fakturerbar"
+                  component={TextField}
+                  margin="dense"
+                  select
                   inputProps={{
                     name: "billable",
                     id: "billable",
                   }}>
                   {billableElements}
-                </Select>
-              </FormControl>
+                </Field>
 
-              <FormControl>
-                <InputLabel htmlFor="responsible">Ansvarlig</InputLabel>
-                <Select
-                  value={selected}
-                  onChange={e => setSelected(e.target.value as string)}
+                <Field
+                  type="text"
+                  name="responsible"
+                  label="Ansvarlig"
+                  component={TextField}
+                  margin="dense"
+                  select
                   inputProps={{
                     name: "responsible",
                     id: "responsible",
                   }}>
-                  {employeeElements.map(e => (
-                    <MenuItem key={e.value} value={e.value}>
-                      {e.label}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-            </Grid>
-          </DialogContent>
-        </>
-      )}
-      <DialogActions>
-        <Button onClick={onClose} variant="contained" color="primary">
-          Avbryt
-        </Button>
-        <Button onClick={onSave} variant="contained" color="primary">
-          Lagre
-        </Button>
-      </DialogActions>
+                  {employeeElements}
+                </Field>
+              </Grid>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={onClose} variant="contained" color="primary">
+                Avbryt
+              </Button>
+              <Button type="submit" variant="contained" color="primary">
+                Lagre
+              </Button>
+            </DialogActions>
+          </Form>
+        )}
+      />
     </Dialog>
   );
 };
