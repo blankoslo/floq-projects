1cea12c861ce11b7278e522d3a498c83787bf1b6
Michael Eikeland <michael.eikeland@blank.no>
2019-10-03 13:48:03 +0200

    Remove state management out of modal

--- src/common/floq/components/FloqModal/FloqModal.tsx
+++ src/common/floq/components/FloqModal/FloqModal.tsx
@@ -5,23 +5,22 @@ type FloqModalProps = {
   open?: boolean;
   title?: string;
   children: React.ReactNode;
+  onClose: () => void;
 };
 const FloqModal: React.FC<FloqModalProps> = (props: FloqModalProps) => {
-  const [isOpen, setOpen] = useState<boolean>(props.open || false);
-
-  if (!isOpen) {
+  if (!props.open) {
     return null;
   }
 
   return (
-    <div className={modalStyles.overlay} onClick={(): void => setOpen(false)}>
+    <div className={modalStyles.overlay} onClick={props.onClose}>
       <div
         className={modalStyles.modal}
         onClick={(e): void => e.stopPropagation()}>
         <div className={modalStyles.header}>
           {props.title && <span>{props.title}</span>}
           <div className={modalStyles.close}>
-            <i className="material-icons" onClick={(): void => setOpen(false)}>
+            <i className="material-icons" onClick={props.onClose}>
               close
             </i>
           </div>
